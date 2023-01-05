2bb229f5e679cfadc8a4cc75c9bad540d00e76fb
Melika Norouzbeygi <melikanorouzbeygi75@gmail.com>
2023-01-04 18:36:53 -0700

    chore: add tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import styles from "../styles/Home.module.css";
-
 
 export default function Home() {
 	return (
@@ -13,8 +9,8 @@ export default function Home() {
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-			<main className={styles.main}>
-				<h2>Hello World!</h2>
+			<main>
+				<h2 className="text-pink-300">Hello World!</h2>
 			</main>
 		</>
 	);
