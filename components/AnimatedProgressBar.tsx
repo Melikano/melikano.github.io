c335791adb1fde95334825762495965612b14d05
Melika Norouzbeygi <melikanorouzbeygi75@gmail.com>
2023-01-21 17:15:09 -0700

    fix(AnimatedProgressBar): fix mobile view

--- components/AnimatedProgressBar.tsx
+++ components/AnimatedProgressBar.tsx
@@ -1,21 +1,25 @@
 import { FC, ReactElement, useEffect, useState } from "react";
+import useGetCurrentBreakpoint from "../helpers/useGetCurrentBreakpoint";
 
 type AnimatedProgressBarProps = { percent: number };
 
 const AnimatedProgressBar: FC<AnimatedProgressBarProps> = ({ percent }) => {
 	const [width, setWidth] = useState(0);
 
+	const { isDesktop } = useGetCurrentBreakpoint();
 	useEffect(() => {
 		setWidth(normalize(percent));
 	}, []);
 
-	const normalize = (percent: number) => percent * 4;
+	const normalize = (percent: number) => percent * (isDesktop ? 4 : 3.7);
 
 	return (
-		<div
-			className="bg-red transition-all duration-1000 delay-300 h-4"
-			style={{ width }}
-		></div>
+		<div className="bg-white h-4" style={{ width: isDesktop ? 400 : "100%" }}>
+			<div
+				className="bg-red transition-all duration-1000 delay-300 h-4"
+				style={{ width }}
+			></div>
+		</div>
 	);
 };
 
